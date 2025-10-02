b056195da51b44617fe06022017c8cf21cf4c3c6
Sajid Alam <msasajid01.pro@gmail.com>
2025-10-02 08:17:26 +0530

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const Login = ({ handlelogin, login }) => {
+const Login = ({ handlelogin, loginerror }) => {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
